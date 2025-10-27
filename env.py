2e4884282ac5808a0c8eb1bd37c254a4084eea3b
immortal-music <emoji2292299@gmail.com>
2025-10-27 15:50:21 +0630

    Update env.py

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "8257255279:AAFp6kNzbb-KGjAykevQeuteGci_uJ8Nwds"
 ADMIN_ID = 1318826936 # သင်၏ Telegram Admin ID
-ADMIN_GROUP_ID = -100123456789 # သင်၏ Admin Group ID
+ADMIN_GROUP_ID = -1002356385851 # သင်၏ Admin Group ID
 
 # ⬇️*** MongoDB URL ကို ဤနေရာတွင် ထည့်ပါ ***⬇️
 # ဥပမာ: "mongodb+srv://user:password@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority"
